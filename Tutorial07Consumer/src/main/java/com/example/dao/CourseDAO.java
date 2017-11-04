792386b03ff2ffe2cf1da621d04e47f1373d4700
Jessica Susanto <jessciacia1097@gmail.com>
2017-11-04 12:52:32 +0700

    all done

---
+++ Tutorial07Consumer/src/main/java/com/example/dao/CourseDAO.java
@@ -0,0 +1,12 @@
+package com.example.dao;
+
+import java.util.List;
+
+import com.example.model.CourseModel;
+
+public interface CourseDAO 
+{
+	CourseModel selectCourse (String id_course);
+	
+	List<CourseModel> selectAllCourses ();
+}
